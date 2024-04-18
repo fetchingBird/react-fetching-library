5a3d40781c0329e9a984edd9e520a4cfe420c380
junparkk <phjun0324@gmail.com>
2024-04-18 20:10:23 +0900

    chore: 프리티어 초기설정

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+const config = {
+  trailingComma: "es5", // ES5에서 유효한 위치에 쉼표를 추가합니다.
+  tabWidth: 2, // 탭 너비를 2로 설정합니다.
+  semi: true, // 세미콜론을 자동으로 추가하지 않습니다.
+  singleQuote: true, // 문자열을 정의할 때 단일 따옴표를 사용합니다.
+  printWidth: 80, // 한 줄의 최대 길이를 80으로 설정합니다.
+};
+
+module.exports = config;
