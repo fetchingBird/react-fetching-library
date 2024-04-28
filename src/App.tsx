dada3cd05453b9f91da19ca059b82f9f16e28ca7
qomi <energyy044@gmail.com>
2024-04-28 14:34:31 +0900

    feat: app.tsx에 queryclient셋업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import './App.css';
+import { QueryClient } from './client/queryClient';
+import { QueryClientProvider } from './context/QueryClientProvider';
 
 function App() {
   const [count, setCount] = useState(0);
+  const queryClient = new QueryClient();
 
-  const {} = useQuery();
-  return <div />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>HI welcome to my homepage</div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
