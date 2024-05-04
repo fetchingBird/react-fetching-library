8672b4b0d3c9638143b1e4041fa6c50f60917ad0
qomi <energyy044@gmail.com>
2024-05-04 13:53:34 +0900

    fix: app tsx에 테스트 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { useState } from 'react';
 import './App.css';
 import { QueryClient } from './client/queryClient';
 import { QueryClientProvider } from './context/QueryClientProvider';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>HI welcome to my homepage</div>
+      <Example />
     </QueryClientProvider>
   );
 }
 
-export default App;
+function Example() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+}
