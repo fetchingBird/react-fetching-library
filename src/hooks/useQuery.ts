f8946c9d91df5582302eeee1c8d29cebcc97c193
qomi <energyy044@gmail.com>
2024-05-15 17:21:01 +0900

    fix: 캐싱 로직 추가

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,23 +1,58 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useCacheStore } from '../context/CacheStoreProvider';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { CacheDataArgs, useCacheStore } from '../context/CacheStoreProvider';
+
+const isAxiosError = <ResponseDataType>(
+  error: unknown
+): error is AxiosError<ResponseDataType> => {
+  return axios.isAxiosError(error);
+};
+
+const hasArrayKey = (map: Map<string[], CacheDataArgs>, keyArray: string[]) => {
+  return Array.from(map.keys()).some(
+    (key) =>
+      Array.isArray(key) &&
+      key.length === keyArray.length &&
+      key.every((val, index) => val === keyArray[index])
+  );
+};
+
+const getLatestDataFromMap = (
+  map: Map<string[], CacheDataArgs>,
+  keyArray: string[]
+): CacheDataArgs | null => {
+  return Array.from(map.entries()).reduce((latest, [key, value]) => {
+    if (
+      Array.isArray(key) &&
+      key.length === keyArray.length &&
+      key.every((val, index) => val === keyArray[index])
+    ) {
+      if (!latest || value.createAt > latest.createAt) {
+        return value;
+      }
+    }
+    return latest;
+  }, null as CacheDataArgs | null);
+};
 
 interface UseQueryArgs {
   queryKey: string[];
-  queryFn: () => void;
+  queryFn: () => Promise<AxiosResponse<any>>;
   cacheTime?: number;
   retry?: number | ((failureCount: number, error: any) => boolean);
 }
 
-const initialCacheTime = 5 * 60 * 1000;
+const INITIAL_CACHE_TIME = 5 * 60 * 1000;
 
 export const useQuery = ({
   queryKey,
   queryFn,
-  cacheTime = initialCacheTime,
+  cacheTime = INITIAL_CACHE_TIME,
   retry = 0,
 }: UseQueryArgs) => {
   const { cacheStore } = useCacheStore();
-  const [initData, setInitData] = useState(null);
+
+  const [initData, setInitData] = useState<AxiosResponse | null>(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState<null | unknown>(null);
   const [isError, setIsError] = useState(false);
@@ -38,12 +73,10 @@ export const useQuery = ({
   );
 
   const fetchWithCache = useCallback(async () => {
-    if (cacheStore.has(queryKey)) {
-      const cache = cacheStore.get(queryKey);
-
+    if (hasArrayKey(cacheStore, queryKey)) {
+      const cache = getLatestDataFromMap(cacheStore, queryKey);
       if (cache) {
         if (Date.now() - cache.createAt < cacheTime) {
-          setInitData(cache.data);
           return;
         }
       }
@@ -52,27 +85,31 @@ export const useQuery = ({
       setIsPending(true);
 
       const data = await queryFn();
-      if (!data.ok) {
-        if (data.status === 404 || data.status === 500) {
-          setFailureCount(failureCount + 1);
-        }
-        // 실패한 경우
-        setIsError(true);
-        setError(data.statusText);
-      }
       cacheStore.set(queryKey, { data, createAt: Date.now() });
 
       setInitData(data);
-    } catch (e: unknown) {
-      // TODO: type 변경
+    } catch (axiosError) {
+      if (isAxiosError(axiosError)) {
+        setError(axiosError.message);
+        setIsError(true);
+      }
+      setFailureCount(failureCount + 1);
 
-      if (shouldRetry(e)) {
-        setTimeout(fetchWithCache, Math.min(1000 * 2 ** failureCount, 30000)); // Exponential back-off
+      if (shouldRetry(error)) {
+        setTimeout(fetchWithCache, Math.min(1000 * 2 ** failureCount, 30000));
       }
     } finally {
       setIsPending(false);
     }
-  }, [cacheStore, cacheTime, failureCount, queryFn, queryKey, shouldRetry]);
+  }, [
+    cacheStore,
+    cacheTime,
+    error,
+    failureCount,
+    queryFn,
+    queryKey,
+    shouldRetry,
+  ]);
 
   useEffect(() => {
     fetchWithCache();
