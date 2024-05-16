694b43974e51335156ca6b9806dc0c277667cb77
qomi <energyy044@gmail.com>
2024-05-16 09:17:43 +0900

    fix: fetch base코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,41 @@ import axios from 'axios';
 import { CacheStoreProvider } from './context/CacheStoreProvider';
 import { useQuery } from './hooks/useQuery';
 
-const EXAMPLE_URL = 'https://jsonplaceholder.typicode.com/todos/1';
+const EXAMPLE_URL = 'https://jsonplaceholder.typicddddode.com/todos/1';
 
 const QUERY_KEY = ['getData'];
 
 function Example() {
+  // const { isPending, error, initData } = useQuery({
+  //   queryKey: QUERY_KEY,
+  //   queryFn: async () => {
+  //     const result = await axios.get(EXAMPLE_URL);
+  //     return result;
+  //   },
+  // });
+  //
   const { isPending, error, initData } = useQuery({
-    queryKey: QUERY_KEY,
-    queryFn: async () => {
-      const result = await axios.get(EXAMPLE_URL);
-      return result;
-    },
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then((res) => res.json())
+        .then((json) => {
+          return json;
+        }),
   });
 
+  console.log('1️⃣', initData);
   const data = initData;
 
   if (error) return `An error has occurred: ${error}`;
 
   return (
     <div>
-      {isPending ? (
+      {/* {isPending ? (
         <strong>Loading...</strong>
       ) : (
         <strong>🍴 {data?.data.title}</strong>
-      )}
+      )} */}
     </div>
   );
 }
